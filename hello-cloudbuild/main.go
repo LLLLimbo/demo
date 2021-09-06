e92eb1c81920827d700a7ce273d8087c5b28e689
LLLLimbo <31607003+LLLLimbo@users.noreply.github.com>
2021-09-06 08:23:53 +0800

    pr_1

--- hello-cloudbuild/main.go
+++ hello-cloudbuild/main.go
@@ -10,6 +10,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, 世界")
 }
 
+func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
